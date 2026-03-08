eb55187d31080e9b9ecfada3e493526733012a91
Yellow <106484528+YellowFireFighter@users.noreply.github.com>
2026-03-08 18:34:29 -0500

    Update esp.lua

--- Util/esp.lua
+++ Util/esp.lua
@@ -3,7 +3,7 @@ local esp = { }
 local framework = loadstring(request({Url = "https://raw.githubusercontent.com/YellowFireFighter/Crumbleware-Rewrite/refs/heads/main/Util/framework.lua", Method = "Get"}).Body)()({debug = false})
 local font = 1
 if Drawing.Fonts and Drawing.Fonts.new then 
-    font = Drawing.Fonts.new(request({Url = "https://github.com/YellowFireFighter/Crumbleware-Rewrite/raw/refs/heads/main/Util/font.ttf", Method = "Get"}).Body)
+    --font = Drawing.Fonts.new(request({Url = "https://github.com/YellowFireFighter/Crumbleware-Rewrite/raw/refs/heads/main/Util/font.ttf", Method = "Get"}).Body)
 end
 
 local workspace = framework.services.workspace
