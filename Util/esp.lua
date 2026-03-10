a9b77379ebaa3a222b32bc7f5a05e03bfc5e6094
Yellow <106484528+YellowFireFighter@users.noreply.github.com>
2026-03-09 21:31:58 -0500

    Add error handling for font registration
    Wrap font registration in pcall to handle errors gracefully.

--- Util/esp.lua
+++ Util/esp.lua
@@ -2,8 +2,10 @@ local esp = { }
 
 local framework = loadstring(request({Url = "https://raw.githubusercontent.com/YellowFireFighter/Crumbleware-Rewrite/refs/heads/main/Util/framework.lua", Method = "Get"}).Body)()({debug = false})
 local font = 2
-if DrawFont ~= nil and DrawFont.Register ~= nil then 
-    font = DrawFont.Register(request({Url = "https://github.com/YellowFireFighter/Crumbleware-Rewrite/raw/refs/heads/main/Util/ProggyClean.ttf", Method = "Get"}).Body)
+if DrawFont ~= nil and DrawFont.Register ~= nil then
+    pcall(function()
+        font = DrawFont.Register(request({Url = "https://github.com/YellowFireFighter/Crumbleware-Rewrite/raw/refs/heads/main/Util/ProggyClean.ttf", Method = "Get"}).Body)
+    end)
 end
 
 local workspace = framework.services.workspace
