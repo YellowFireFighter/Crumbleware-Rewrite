4956303b3286bfa75e8a0576b7d60bba4f6f259f
Yellow <106484528+YellowFireFighter@users.noreply.github.com>
2026-03-15 13:41:23 -0500

    Update esp.lua

--- Util/esp.lua
+++ Util/esp.lua
@@ -2,7 +2,7 @@ local esp = { }
 
 local framework = loadstring(request({Url = "https://raw.githubusercontent.com/YellowFireFighter/Crumbleware-Rewrite/refs/heads/main/Util/framework.lua", Method = "Get"}).Body)()({debug = false})
 local font = 2
-if DrawFont ~= nil and DrawFont.Register ~= nil then 
+if identifyexecutor and identifyexecutor():find("Synapse Z") then 
     pcall(function()
         font = DrawFont.Register(request({Url = "https://github.com/YellowFireFighter/Crumbleware-Rewrite/raw/refs/heads/main/Util/ProggyClean.ttf", Method = "Get"}).Body)
     end)
